743f0bcc290963a3c5f76fd8ce3fb737db4460a3
SophieWardle <sophiewardle@ymail.com>
2023-03-13 12:08:07 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -29,13 +28,6 @@ class App extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
 
 export default App;
