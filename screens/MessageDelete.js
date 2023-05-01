774ad03bedf2c76dec6a9ddd59e0285f3f134d18
SophieWardle <sophiewardle@ymail.com>
2023-05-01 02:18:00 +0100

    Message update/delete linted

--- screens/MessageDelete.js
+++ screens/MessageDelete.js
@@ -1,58 +1,55 @@
-import React, { Component } from "react";
-import { View} from "react-native";
-//API
-import { deleteChatMessage } from "../api/ChatManagement";
-//MY COMPONENTS
-import ConfirmTask from "../components/ConfirmTask";
-//STYLES
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
+/* eslint-disable react/prop-types */
+import React, { Component } from 'react';
+import { View } from 'react-native';
+// API
+import { deleteChatMessage } from '../api/ChatManagement';
+// MY COMPONENTS
+import ConfirmTask from '../components/ConfirmTask';
+// STYLES
 import styles from '../styles/globalTheme';
 
 export default class MessageDelete extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            message_id: this.props.route.params.message_id,
-            chat_id: this.props.route.params.chat_id
-        };
-        console.log("Message id delete:" + this.state.message_id + "chat_id delete:" + this.state.chat_id)
-    }
-
-    handleCancel = () => {
-        this.props.navigation.goBack();
+    this.state = {
+      messageId: props.route.params.message_id,
+      chatId: props.route.params.chat_id,
     };
+  }
 
-    handleConfirm = () => {
-        this.handleDelete();
-    };
+  handleCancel = () => {
+    const navigation = this.props;
+    navigation.navigation.goBack();
+  };
 
-    async handleDelete()  {
-        const {chat_id, message_id} = this.state;
-        console.log(chat_id,message_id);
-        deleteChatMessage(chat_id, message_id)
-        .then((responseJson) => {
-            console.log(responseJson);
-            this.setState({
-              isLoading: false,
-              chats: responseJson
-            });
-            this.props.navigation.goBack();
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-    }
+  handleConfirm = () => {
+    this.handleDelete();
+  };
 
-    render() {
-        const {message_id, chat_id} = this.state;
-        return (
-            <View style={styles.backgroundContainer}>
-                <ConfirmTask
-                    message="delete this message"
-                    onCancel={this.handleCancel}
-                    onConfirm={this.handleConfirm}
-                />
-            </View>
-        );
-    }
+  async handleDelete() {
+    const { chatId, messageId } = this.state;
+    const navigation = this.props;
+    deleteChatMessage(chatId, messageId)
+      .then(() => {
+        navigation.navigation.goBack();
+      })
+      .catch((err) => {
+        console.log(`error${err}`);
+      });
+  }
+
+  render() {
+    return (
+      <View style={styles.backgroundContainer}>
+        <ConfirmTask
+          message="delete this message"
+          onCancel={this.handleCancel}
+          onConfirm={this.handleConfirm}
+        />
+      </View>
+    );
+  }
 }
--- screens/MessageUpdate.js
+++ screens/MessageUpdate.js
@@ -1,80 +1,89 @@
-import React, { Component } from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
-import { TouchableOpacity } from "react-native";
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
+/* eslint-disable react/prop-types */
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
-//API
-import { updateChatMessage } from "../api/ChatManagement";
-export default class MessageUpdate extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            message: this.props.route.params.message,
-            message_id: this.props.route.params.message_id,
-            chat_id: this.props.route.params.chat_id
-        };
-        console.log("Message id update:" + this.state.message_id + "chat_id update:" + this.state.chat_id)
-    }
-
-    handleUpdate = async (chat_id, message_id) =>  {
-        const to_send = {};
-
-        if (this.state.message !== ""){
-            to_send.message = this.state.message;
-        }
-        //TO SEND
-
-
-        console.log(chat_id,message_id);
-        updateChatMessage(chat_id, message_id, to_send)
-        .then((responseJson) => {
-            console.log(responseJson);
-            this.setState({
-              isLoading: false,
-              error: "updated message success"
-            });
-            this.props.navigation.goBack();
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-    }
-
-    messageHandler = (msg) => {
-        this.setState({ message: msg});
-    }
-    
-    render() {
-        return (
-            <View>
-                <TextInput
-                  value={this.state.message} 
-                  onChangeText={this.messageHandler}
-                />
-                 <Text style={styles.errorMessage}>{this.state.error}</Text>
-                    <TouchableOpacity onPress={() => this.handleUpdate(this.state.chat_id, this.state.message_id)}>
-                        <View style={styles.button}>
-                            <Text style={styles.buttonText}>Update Message</Text>
-                        </View>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => this.props.navigation.goBack()}>
-                        <View style={styles.button}>
-                            <Text style={styles.buttonText}>Back</Text>
-                        </View>
-                    </TouchableOpacity>
-            </View>
-        );
-    }
-}
-
+// API
+import { updateChatMessage } from '../api/ChatManagement';
 
 const styles = StyleSheet.create({
-    contactsContainer: {
-        flex: 1,
-        backgroundColor: '#fff',
-        paddingHorizontal: 16,
-        paddingVertical: 8,
-    },
-    
+  contactsContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+  },
 
-})
+});
+
+export default class MessageUpdate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: props.route.params.message,
+      messageId: props.route.params.message_id,
+      chatId: props.route.params.chat_id,
+    };
+  }
+
+  handleUpdate = async (chatId, messageId) => {
+    const toSend = {};
+    const { message } = this.state;
+    const navigation = this.props;
+
+    if (message !== '') {
+      toSend.message = message;
+    }
+    updateChatMessage(chatId, messageId, toSend)
+      .then(() => {
+        this.setState({
+          error: 'updated message success',
+        });
+        navigation.navigation.goBack();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  messageHandler = (msg) => {
+    this.setState({ message: msg });
+  };
+
+  render() {
+    const {
+      message,
+      chatId,
+      messageId,
+      error,
+    } = this.state;
+    const navigation = this.props;
+    return (
+      <View>
+        <TextInput
+          value={message}
+          onChangeText={this.messageHandler}
+        />
+        <Text style={styles.errorMessage}>{error}</Text>
+        <TouchableOpacity onPress={() => this.handleUpdate(chatId, messageId)}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Update Message</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigation.goBack()}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Back</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
